7ef9dafba8d7c3139bbc8277e6b2d3ab961c61bf
￴￴ <>
2024-05-25 23:14:41 +0700

    update

--- package/telegram_client/bin/tdlib_docs_to_json.dart
+++ package/telegram_client/bin/tdlib_docs_to_json.dart
@@ -5,58 +5,65 @@ import 'package:general_lib/general_lib.dart';
 void main(List<String> args) async {
   String params = """
 
-bool 	use_test_dc_
- 	Pass true to use Telegram test environment instead of the production environment.
+int64 	id_
+ 	Session identifier.
  
-string 	database_directory_
- 	The path to the directory for the persistent database; if empty, the current working directory will be used.
+bool 	is_current_
+ 	True, if this session is the current session.
  
-string 	files_directory_
- 	The path to the directory for storing files; if empty, database_directory will be used.
+bool 	is_password_pending_
+ 	True, if a 2-step verification password is needed to complete authorization of the session.
  
-bytes 	database_encryption_key_
- 	Encryption key for the database. If the encryption key is invalid, then an error with code 401 will be returned.
+bool 	is_unconfirmed_
+ 	True, if the session wasn't confirmed from another session.
  
-bool 	use_file_database_
- 	Pass true to keep information about downloaded and uploaded files between application restarts.
+bool 	can_accept_secret_chats_
+ 	True, if incoming secret chats can be accepted by the session.
  
-bool 	use_chat_info_database_
- 	Pass true to keep cache of users, basic groups, supergroups, channels and secret chats between restarts. Implies use_file_database.
+bool 	can_accept_calls_
+ 	True, if incoming calls can be accepted by the session.
  
-bool 	use_message_database_
- 	Pass true to keep cache of chats and messages between restarts. Implies use_chat_info_database.
- 
-bool 	use_secret_chats_
- 	Pass true to enable support for secret chats.
+object_ptr< SessionType > 	type_
+ 	Session type based on the system and application version, which can be used to display a corresponding icon.
  
 int32 	api_id_
- 	Application identifier for Telegram API access, which can be obtained at https://my.telegram.org.
+ 	Telegram API identifier, as provided by the application.
  
-string 	api_hash_
- 	Application identifier hash for Telegram API access, which can be obtained at https://my.telegram.org.
- 
-string 	system_language_code_
- 	IETF language tag of the user's operating system language; must be non-empty.
- 
-string 	device_model_
- 	Model of the device the application is being run on; must be non-empty.
- 
-string 	system_version_
- 	Version of the operating system the application is being run on. If empty, the version is automatically detected by TDLib.
+string 	application_name_
+ 	Name of the application, as provided by the application.
  
 string 	application_version_
- 	Application version; must be non-empty.
+ 	The version of the application, as provided by the application.
+ 
+bool 	is_official_application_
+ 	True, if the application is an official application or uses the api_id of an official application.
+ 
+string 	device_model_
+ 	Model of the device the application has been run or is running on, as provided by the application.
+ 
+string 	platform_
+ 	Operating system the application has been run or is running on, as provided by the application.
+ 
+string 	system_version_
+ 	Version of the operating system the application has been run or is running on, as provided by the application.
+ 
+int32 	log_in_date_
+ 	Point in time (Unix timestamp) when the user has logged in.
+ 
+int32 	last_active_date_
+ 	Point in time (Unix timestamp) when the session was last used.
+ 
+string 	ip_address_
+ 	IP address from which the session was created, in human-readable format.
+ 
+string 	location_
+ 	A human-readable description of the location from which the session was created, based on the IP address.
 """
       .trim();
 
   Map parameters_data = {};
 
-  List<RegExpMatch> parameters_regex = params
-      .split("\n")
-      .map((e) => RegExp("^(([a-z]+)([ \t])+((.*)(_)))", caseSensitive: false)
-          .firstMatch(e))
-      .whereType<RegExpMatch>()
-      .toList();
+  List<RegExpMatch> parameters_regex = params.split("\n").map((e) => RegExp("^(([a-z0-9]+)([ \t])+((.*)(_)))", caseSensitive: false).firstMatch(e)).whereType<RegExpMatch>().toList();
   for (var i = 0; i < parameters_regex.length; i++) {
     RegExpMatch regExpMatch = parameters_regex[i];
     String value = regExpMatch.group(2) ?? "";
@@ -68,6 +75,12 @@ string 	application_version_
           return false;
         }
         return true;
+      }
+      if (value == "int64") {
+        return 0;
+      }
+      if (value == "int32") {
+        return 0;
       }
       if (value == "bytes") {
         return "";
